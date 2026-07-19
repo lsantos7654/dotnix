ee3182a83fe094f5f498a56568f354aac770f685
Lucas Santos <lsantos7654@gmail.com>
2026-07-19 01:28:10 -0400

    fix: Re-enable neovim on Asahi host

--- modules/asahi.nix
+++ modules/asahi.nix
@@ -1,13 +1,12 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.asahi = { lib, pkgs, ... }: {
+  flake.modules.nixos.asahi = { pkgs, ... }: {
     imports = [ inputs.apple-silicon.nixosModules.apple-silicon-support ];
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = false;
 
     hardware.asahi.enable = true;
-    home-manager.users.santos.programs.neovim.enable = lib.mkForce false;
 
     services.envfs.enable = true;
 
